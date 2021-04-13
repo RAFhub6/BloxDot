d90ee8a63882c3fe209ad5879234c47bebae0dbc
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2021-04-13 12:31:03 +1000

    Update dialog.js

--- resources/dialog.js
+++ resources/dialog.js
@@ -1,2 +1,12 @@
 console.log("this is in beta,this might not work")
 alert("this is in beta so some things might not work")
+<div id="x"></div>
+
+<script>
+var img = document.createElement("img");
+ 
+img.src = "bdot.png";
+var src = document.getElementById("x");
+ 
+src.appendChild(img);
+</script>
